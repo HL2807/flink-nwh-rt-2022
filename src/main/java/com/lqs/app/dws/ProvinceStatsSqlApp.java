27b0d552c9deda3e0d07a376f3046d0c80bf4187
lqs <1450640631@qq.com>
2022-03-15 01:17:29 +0800

    Initial commit

---
+++ src/main/java/com/lqs/app/dws/ProvinceStatsSqlApp.java
@@ -0,0 +1,11 @@
+package com.lqs.app.dws;
+
+/**
+ * @Author lqs
+ * @Date 2022年03月15日 01:15:31
+ * @Version 1.0.0
+ * @ClassName ProvinceStatsSqlApp
+ * @Describe 地区主题表
+ */
+public class ProvinceStatsSqlApp {
+}
